cc5cf32e28376d9721e1d76dab0f7cc738261416
JorgeJr <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2024-02-27 20:32:52 -0300

    criada classe pessoa e salvo em um json

---
+++ Ex-Salvar_classe_em_json.py
@@ -0,0 +1,20 @@
+import json
+
+DESTINO = "classe-aula127.json"
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+p1 = Pessoa("Joao", 23)
+p2 = Pessoa("Ana", 27)
+p3 = Pessoa("Helena", 13)
+
+
+bd = [vars(p1), vars(p2), vars(p3)]
+
+with open(DESTINO, "w") as arquivo:
+    json.dump(bd, arquivo, ensure_ascii=False, indent=2)
