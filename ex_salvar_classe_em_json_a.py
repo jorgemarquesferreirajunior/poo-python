a307109c584af666a30148c644855a132fd7eb95
JorgeJr <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2024-02-27 20:53:28 -0300

    usando if __name__

--- ex_salvar_classe_em_json_a.py
+++ ex_salvar_classe_em_json_a.py
@@ -16,7 +16,15 @@ p3 = Pessoa("Helena", 13)
 
 bd = [vars(p1), vars(p2), vars(p3)]
 
+
 def fazer_dump():
     with open(DESTINO, "w") as arquivo:
         print("FAZENDO DUMP")
         json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+
+# Fazer isso garante que a funcao fazer_dump sera chamada apenas quando o script da funcao for executado
+# Caso esse script seja importado para outro, a funcao fazer_dump nao sera chamada automaticamente
+if __name__ == "__main__":
+    print("Executando o script ex_salvar_classe_em_json_a.py")
+    fazer_dump()
