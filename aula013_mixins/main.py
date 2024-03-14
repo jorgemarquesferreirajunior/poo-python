217a8c8d92e363a4ff69349ac959b948d712e5c2
JorgeJr <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2024-03-13 21:38:46 -0300

    heranca multipla

--- aula013_mixins/main.py
+++ aula013_mixins/main.py
@@ -1,9 +1,18 @@
-from log import LogPrintMixin, LogFileMixin
+# from log import LogPrintMixin, LogFileMixin
 
-log = LogPrintMixin()
-log.log_error("qualquer coisa")
-log.log_success("Que legal")
+# log = LogPrintMixin()
+# log.log_error("qualquer coisa")
+# log.log_success("Que legal")
 
-lf = LogFileMixin()
-lf.log_error("qualquer coisa")
-lf.log_success("Que legal")
+# lf = LogFileMixin()
+# lf.log_error("qualquer coisa")
+# lf.log_success("Que legal")
+
+
+from eletronico import Smartphone
+
+xiaomi = Smartphone("Xiaomi 10S")
+iphone = Smartphone("Iphone 14")
+
+xiaomi.ligar()
+iphone.desligar()
